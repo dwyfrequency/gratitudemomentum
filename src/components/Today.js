7a9ea25128628343b775e5467f4f4b333ad6294c
dwyfrequency <jdwy215@gmail.com>
2018-11-28 07:47:11 -0500

    added checkbox but need to debug why it will nt be checked and update state

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 
 class Today extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isAcknowledged: false };
+  }
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
   render() {
     const { gratEntry } = this.props;
+    const { isAcknowledged } = this.state;
     return (
       <div>
         <h3>Today</h3>
-        <p>
-          <strong>{gratEntry}</strong>
-        </p>
+        <form>
+          <label>
+            <input
+              name="isAcknowledged"
+              type="checkbox"
+              checked={isAcknowledged}
+            />
+            <strong>{gratEntry}</strong>
+          </label>
+        </form>
       </div>
     );
   }
