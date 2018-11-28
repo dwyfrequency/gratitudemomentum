6c8c14bbba2f12063dd6d138e0ad163fb7861296
dwyfrequency <jdwy215@gmail.com>
2018-11-27 21:11:42 -0500

    adding initial skeleton for gratform

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Dashboard from "./Dashboard";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div className="App">
