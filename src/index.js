65c7af6b052c5d054e4ada9f4a3a167ad9939888
dwyfrequency <jdwy215@gmail.com>
2018-11-30 19:09:13 -0500

    added css to remove focus on grat entry input

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App.js";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
