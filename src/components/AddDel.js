1b8f481fd5c9a9c6efb52a3fa6634c8ae912a1a3
dwyfrequency <jdwy215@gmail.com>
2018-12-01 08:11:07 -0500

    added react-icons

--- src/components/AddDel.js
+++ src/components/AddDel.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { MdClose } from "react-icons/md";
 
 export default function AddDel(props) {
-  return <span>X</span>;
+  return (
+    <button>
+      <MdClose />
+    </button>
+  );
 }
