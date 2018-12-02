746e35109b000e419d3ba956c51a10cee0a68345
dwyfrequency <jdwy215@gmail.com>
2018-12-02 06:11:38 -0500

    adding all files

--- src/components/DailyQuote.js
+++ src/components/DailyQuote.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
+import API from "../funcs/API";
 
 class DailyQuote extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  componentDidMount = () => {
+    const { text, from: author } = API("../data/enterpreneur-quotes.json");
+    this.setState({ text, author });
+  };
+
   render() {
-    return <footer>b</footer>;
+    const { text, author } = this.state;
+    return (
+      <footer>
+        <p>{author}:</p>
+        <p>{text}</p>
+      </footer>
+    );
   }
 }
 
--- src/funcs/API.js
+++ src/funcs/API.js
@@ -1,12 +1,17 @@
 export default function API(path) {
+  console.log(path);
   function getRandomIntInclusive(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - 0 + 1)) + 0; //The maximum is inclusive and the minimum is inclusive
   }
   return fetch(path)
-    .then(resp => resp.json())
+    .then(resp => console.log(resp.text()))
     .then(data => {
       const lenLessOne = data.length - 1;
       return data[getRandomIntInclusive(lenLessOne)];
     });
+  // .catch(err => {
+  //   console.warn(err);
+  //   return null;
+  // });
 }
