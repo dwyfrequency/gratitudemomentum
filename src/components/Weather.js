9312c298f25c7d47d496bb8eede9ca25d9fc0db0
dwyfrequency <jdwy215@gmail.com>
2018-12-02 14:39:10 -0500

    adding weather fetching code

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
+import config from "../hidden/config";
 
 class Weather extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      city: this.props.city ? this.props.city : "Miami",
+      countryCode: this.props.countryCode ? this.props.countryCode : "US"
+    };
+    this.getWeather = this.getWeather.bind(this);
+    this.setWeather = this.setWeather.bind(this);
   }
-  componentDidMount = () => {};
+
+  async getWeather() {
+    const { WEATHER_KEY } = config;
+    const { city, countryCode } = this.state;
+    // URL
+    const weatherEndpoint = `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=imperial&appid=${WEATHER_KEY}`;
+    // log(weatherEndpoint);
+
+    const weatherResps = await fetch(weatherEndpoint);
+    const responseData = await weatherResps.json();
+    return responseData;
+  }
+
+  async setWeather() {
+    const apiResponse = await this.getWeather();
+    console.log(apiResponse);
+    const { description, main: descrSummary, icon } = apiResponse.weather[0];
+    const { humidity, temp } = apiResponse.main;
+    this.setState({
+      description,
+      descrSummary,
+      icon: `http://openweathermap.org/img/w/${icon}.png`,
+      humidity,
+      temp
+    });
+  }
+
+  componentDidMount = () => {
+    this.setWeather();
+  };
 
   render() {
     return <div>weather</div>;
---
+++ src/hidden/config.js
@@ -0,0 +1,6 @@
+// https://home.openweathermap.org/api_keys
+const config = {
+  WEATHER_KEY: "f84a3508a700636d53a68a8705225746"
+};
+
+export default config;
